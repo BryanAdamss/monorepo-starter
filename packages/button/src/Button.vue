81d933630876bd2366c7028fd36c646dec166569
bryanadamss <bryanadamss@foxmail.com>
2021-02-07 11:27:50 +0800

    chore(button): 调整button组件逻辑

---
+++ packages/button/src/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="c-Button">我是按钮</button>
+</template>
+
+<script>
+/**
+ * * Button
+ */
+
+export default {
+  name: 'Button',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style src="button.css"></style>
