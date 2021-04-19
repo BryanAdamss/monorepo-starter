a0bcdf0a003c3e93435960944527c4c977d1fb83
bryanadamss <bryanadamss@foxmail.com>
2021-04-19 20:43:37 +0800

    feat(doc): 支持docjs

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,1 +1,16 @@
-export const print = (msg) => `Hello ${msg}`
+/**
+ * @author GuangHui
+ * @description utils方法
+ */
+
+/**
+ * 生成欢迎信息
+ *
+ * @date 2021-04-19 20:39:49
+ * @export
+ * @param {string} msg 信息
+ * @return {string} 欢迎信息
+ */
+export function print(msg) {
+  return `Hello ${msg}`
+}
