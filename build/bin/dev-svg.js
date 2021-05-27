f4c038825fc785b408eca3a7f9dc6b7b0b95cfdf
bryanadamss <bryanadamss@foxmail.com>
2021-05-27 19:09:33 +0800

    chore(dev-svg): 优化逻辑

--- build/bin/dev-svg.js
+++ build/bin/dev-svg.js
@@ -9,18 +9,31 @@ const chokidar = require('chokidar')
 const { execSync } = require('child_process')
 const log = console.log.bind(console)
 
-function execCopySvgs(changedPath) {
+function execCopySvgs(changedPath, stats) {
   log(`-------${changedPath} changed-------`)
-  execSync('yarn copy:svgs', {
-    cwd: join(packagesDirName, svgPkgName)
-  })
+
+  try {
+    execSync('yarn copy:svgs', {
+      cwd: join(packagesDirName, svgPkgName)
+    })
+  } catch (error) {
+    log('execSync(\'yarn copy:svgs\')失败')
+  }
 }
 
-chokidar
+const watcher = chokidar
   .watch(`${publicDirName}/svgs/**/*.svg`, {
     persistent: true,
     awaitWriteFinish: {
       stabilityThreshold: 300
     }
   })
-  .on('change', execCopySvgs)
+
+watcher.on('ready', (event, path, details) => { // internal
+  log('chokidar ready')
+
+  watcher
+    .on('add', execCopySvgs)
+    .on('change', execCopySvgs)
+    .on('unlink', execCopySvgs)
+})
